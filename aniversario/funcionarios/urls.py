bc2983a062b337cff6fa8c607337706eeb45f477
unknown <laryssamc.ted@gmail.com>
2025-01-09 17:40:45 -0300

    modal de confirmação de exclusão funcionando, mexendo no icone trash

--- aniversario/funcionarios/urls.py
+++ aniversario/funcionarios/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('importar-funcionarios/', views.importar_funcionarios, name='importar_funcionarios'),
     path('editar-funcionarios/<int:cbo>/', views.editar_funcionarios, name='editar_funcionarios'),
     path('menu-cadastros/', views.menu_cadastros, name='menu_cadastros'),
-    path('excluir-cadastro/<int:cbo>/', views.excluir_funcionario, name='excluir_funcionario'),
+    path('excluirfuncionario/<int:cbo>/', views.excluir_funcionario, name='excluir_funcionario'),
     #path('enviar-email-aniversariantes/', views.enviar_emails_aniversariantes_view, name='enviar_emails_aniversariantes'),
 ]
